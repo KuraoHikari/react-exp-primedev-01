b190195aeebd599bba9a26ef9ea9217f1f8e0b77
KuraoHikari <dewaindra705@gmail.com>
2023-09-14 10:47:10 +0800

    add controller and route middleware, cors setup

--- drizzle-orm-comparison-drizzle/src/db/index.ts
+++ drizzle-orm-comparison-drizzle/src/db/index.ts
@@ -1,5 +1,7 @@
 import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 neonConfig.fetchConnectionCache = true;
 
--- drizzle-orm-comparison-drizzle/src/index.ts
+++ drizzle-orm-comparison-drizzle/src/index.ts
@@ -1,38 +1,50 @@
-import express from 'express';
-import { db } from './db';
-import { users as userSchema } from './db/schema';
-import { eq } from 'drizzle-orm';
+import express from "express";
+import { db } from "./db";
+import { users as userSchema } from "./db/schema";
+import { eq } from "drizzle-orm";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/users', async (req, res) => {
+app.get("/", async (req, res) => {
+  res.json({
+    message: "aman",
+  });
+});
+
+app.get("/users", async (req, res) => {
   const users = await db.select().from(userSchema);
   res.json(users);
 });
 
-app.get('/users/:id', async (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const { id } = req.params;
-  const user = await db.select().from(userSchema).where(eq(userSchema.id, Number(id)));
+  const user = await db
+    .select()
+    .from(userSchema)
+    .where(eq(userSchema.id, Number(id)));
   res.json(user);
 });
 
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   const { name, email } = req.body;
   const user = await db.insert(userSchema).values([{ name, email }]);
   res.json(user);
 });
 
-app.put('/users/:id', async (req, res) => {
+app.put("/users/:id", async (req, res) => {
   const { id } = req.params;
   const { name, email } = req.body;
-  const user = await db.update(userSchema).set({ name, email }).where(eq(userSchema.id, Number(id)));
+  const user = await db
+    .update(userSchema)
+    .set({ name, email })
+    .where(eq(userSchema.id, Number(id)));
   res.json(user);
 });
 
-app.delete('/users/:id', async (req, res) => {
+app.delete("/users/:id", async (req, res) => {
   const { id } = req.params;
   const user = await db.delete(userSchema).where(eq(userSchema.id, Number(id)));
   res.json(user);
